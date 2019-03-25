eeb1d26b07f9f129d4f1e7956b8ec81daf49c61a
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-03-25 19:58:34 +1100

    Current status

--- wallet-poc/src/Cardano/Wallet/ChainSync.hs
+++ wallet-poc/src/Cardano/Wallet/ChainSync.hs
@@ -1,22 +1,47 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE NamedFieldPuns      #-}
-{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Cardano.Wallet.ChainSync
   ( Client (..)
   , syncChain
   ) where
 
+import           Cardano.Prelude hiding (atomically)
+
+import           Codec.Serialise (Serialise (..))
+import           Control.DeepSeq (NFData)
+import qualified Data.ByteArray as ByteArray
+import           Data.FingerTree (Measured (..))
+import qualified Prelude
+
+import           Control.Monad.Class.MonadST (MonadST (..))
 import           Control.Monad.Class.MonadSTM (MonadSTM (..), TVar)
 
-import           Ouroboros.Network.Block (HasHeader (..))
+import           Cardano.Binary.Class (DecoderError (..))
+import           Cardano.Chain.Block (Block, ExtraBodyData, Header)
+import qualified Cardano.Chain.Block as Block
+import           Cardano.Chain.Slotting (EpochSlots (..), flattenSlotId, FlatSlotId (..))
+import           Cardano.Chain.Common (Attributes, BlockCount (..), ChainDifficulty (..))
+import           Cardano.Crypto (AbstractHash (..), Hash, HashAlgorithm, hash)
+import qualified Crypto.Hash
+
+import           Ouroboros.Network.Block (BlockMeasure, BlockNo (..), HasHeader (..), ChainHash (..), SlotNo (..), StandardHash (..), blockMeasure)
 import           Ouroboros.Network.Chain (Chain (..), Point (..), ChainUpdate(..))
 import qualified Ouroboros.Network.Chain as Chain
 import           Ouroboros.Network.ChainProducerState (ChainProducerState, ReaderId)
 import qualified Ouroboros.Network.ChainProducerState as ChainProducerState
-import           Ouroboros.Network.Protocol.ChainSync.Client (ChainSyncClient (..), ClientStIdle (..), ClientStNext (..), ClientStIntersect (..))
+import           Ouroboros.Network.Protocol.ChainSync.Client (ChainSyncClient (..),
+                    ClientStIdle (..), ClientStNext (..), ClientStIntersect (..))
 
 data Client header m t = Client
   { rollbackward :: Point header -> Point header -> m (Either t (Client header m t))
@@ -24,6 +49,44 @@ data Client header m t = Client
   , points       :: [Point header] -> m (Client header m t)
   }
 
+instance Measured BlockMeasure Block where
+  measure = blockMeasure
+
+instance HasHeader Block where
+  type HeaderHash Block = Hash Header
+  blockHash = Block.blockHash
+
+  -- TODO: This does not account for the genesis hash.
+  blockPrevHash = BlockHash . Block.blockPrevHash
+
+  -- TODO: The 'EpochSlots 0' is definitely not correct!
+  blockSlot = SlotNo . unFlatSlotId . flattenSlotId (EpochSlots 0) . Block.blockSlot
+
+  blockNo = BlockNo . getBlockCount . getChainDifficulty . Block.blockDifficulty
+
+  -- Possible to check invariants in this function.
+  blockInvariant = pure True
+
+instance StandardHash Block where
+
+
+-- Lifted from the similar `Bi` instance in cardano-sl-crypto.
+instance (Typeable algo, Typeable a, HashAlgorithm algo) => Serialise (AbstractHash algo a) where
+  encode (AbstractHash digest) =
+    encode (ByteArray.convert digest :: ByteString)
+
+  -- FIXME bad decode: it reads an arbitrary-length byte string.
+  -- Better instance: know the hash algorithm up front, read exactly that
+  -- many bytes, fail otherwise. Then convert to a digest.
+  decode = do
+    bs <- decode @ByteString
+    maybe
+      (cborError $ DecoderErrorCustom
+        "AbstractHash"
+        "Cannot convert ByteString to digest"
+      )
+      (pure . AbstractHash)
+      (Crypto.Hash.digestFromByteString bs)
 
 
 -- | An instance of the client side of the chain sync protocol that
@@ -33,7 +96,7 @@ data Client header m t = Client
 -- this is not a realistic chain representation.
 --
 syncChain
-    :: forall header m a. (HasHeader header, MonadSTM m)
+    :: forall header m a. (HasHeader header, NFData header, MonadSTM m)
     => TVar m (Chain header)
     -> Client header m a
     -> ChainSyncClient header (Point header) m a
@@ -106,3 +169,26 @@ syncChain chainvar client = ChainSyncClient $
 --
 recentOffsets :: [Int]
 recentOffsets = [0,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584]
+
+
+
+
+-- | Testing @'Client'@ which stops at a given point.
+--
+runClient
+  :: MonadSTM m
+  => Point Block
+  -> Client Block m ()
+runClient tip =
+  Client
+    { rollbackward = \point _ ->
+        if point == tip
+          then
+            return $ Left ()
+          else return $ Right (runClient tip)
+    , rollforward = \block ->
+        if Chain.blockPoint block == tip
+          then return $ Left ()
+          else return $ Right (runClient tip)
+    , points = \_ -> return (runClient tip)
+    }
